0135505f41a2a93cabf5fcc017985869f0e6cd41
Oleg Sheynin <oleg1@sheynin.com>
2020-10-12 16:39:48 -0400

    progress

--- coronavirus/covid_19_shared.py
+++ coronavirus/covid_19_shared.py
@@ -52,3 +52,49 @@ def loadJHUData(cntr : str, to_save : bool):
     if to_save:
         df.to_csv(Settings.save_file_name, index=True)
     return df
+
+#------------------------------------------------------
+#------------------------------------------------------
+#------------------------------------------------------
+def loadJHUData_USA(st : str):
+    class Settings:
+        from datetime import datetime
+
+        conf_url="https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
+        deaths_url="https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
+
+        state = st
+
+        dt = datetime.now().strftime("%Y-%m-%d")
+        save_file_name = f'./{state}.{dt}.covid-19.csv'
+
+
+    confirmed = pd.read_csv(Settings.conf_url)
+    deaths = pd.read_csv(Settings.deaths_url)
+
+
+    for df in [deaths, confirmed]:
+        # Filter Country
+        df.drop(df[ df["Province_State"] != Settings.state].index, inplace=True)
+        # Drop Unused Columns:
+        columns_to_delete = range(0, 11)
+        df.drop(df.columns[columns_to_delete], axis=1, inplace=True)
+        if 'Population' in df:
+            del df['Population']
+        df.reset_index()
+    
+    
+    confirmed_ser = confirmed.sum()
+    confirmed_ser.index.names = ["Date"]
+    confirmed_ser.index = pd.to_datetime(confirmed_ser.index)
+
+    deaths_ser = deaths.sum()
+    deaths_ser.index.names = ["Date"]
+    deaths_ser.index = pd.to_datetime(deaths_ser.index)
+    
+    df = pd.DataFrame({
+        "Confirmed" : confirmed_ser
+        , "Dead" : deaths_ser
+                      }
+    )
+    return df
