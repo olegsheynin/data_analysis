5cce624b18d540fc151ebeb40e23aae9bf2bfc03
Oleg Sheynin <oleg1@sheynin.com>
2020-11-10 15:08:24 -0500

    progress

--- covid-19/covid_19_shared.py
+++ covid-19/covid_19_shared.py
@@ -11,27 +11,29 @@ def list_of_JHUD_US_states():
     return res.unique()    
 
 
-#------------------------------------------------------
-def load_JHU_Data_USA(st : str):
-    class Settings:
-        from datetime import datetime
 
+class USA_Data:
+    def __init__(self):
         conf_url = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
         deaths_url = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"
 
-        state = st
-
-        dt = datetime.now().strftime("%Y-%m-%d")
-        save_file_name = f'./{state}.{dt}.covid-19.csv'
-
-
-    confirmed = pd.read_csv(Settings.conf_url)
-    deaths = pd.read_csv(Settings.deaths_url)
+        self.confirmed_ = pd.read_csv(conf_url)
+        self.deaths_ = pd.read_csv(deaths_url)
+        self.recovered_ = None
+    
+#------------------------------------------------------
+def usa_state_data(state : str, usa_data = None):
+    
+    if usa_data is None:
+        usa_data = USA_Data()
+    confirmed = usa_data.confirmed_.copy()
+    deaths = usa_data.deaths_.copy()
+    
 
 
     for df in [deaths, confirmed]:
         # Filter Country
-        df.drop(df[ df["Province_State"] != Settings.state].index, inplace=True)
+        df.drop(df[ df["Province_State"] != state].index, inplace=True)
         # Drop Unused Columns:
         columns_to_delete = range(0, 11)
         df.drop(df.columns[columns_to_delete], axis=1, inplace=True)
@@ -55,7 +57,6 @@ def load_JHU_Data_USA(st : str):
     )
     return df
 
-
 def list_of_JHU_countries():
     conf_url="https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
     df = pd.read_csv(conf_url)
@@ -63,28 +64,29 @@ def list_of_JHU_countries():
     return countries.unique()    
 
 
-def load_JHU_Data_global(st : str):
-    class Settings:
-        from datetime import datetime
-
+class Global_Data:
+    def __init__(self):
         conf_url="https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
         rcvrd_url="https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
         deaths_url="https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
-        country = st
-
-        dt = datetime.now().strftime("%Y-%m-%d")
-        save_file_name = f'./{country}.{dt}.covid-19.csv'
-
-
-    confirmed = pd.read_csv(Settings.conf_url)
-    deaths = pd.read_csv(Settings.deaths_url)
-    recs = pd.read_csv(Settings.rcvrd_url)
+        self.confirmed_ = pd.read_csv(conf_url)
+        self.deaths_ = pd.read_csv(deaths_url)
+        self.recovered_ = pd.read_csv(rcvrd_url)
 
 
+# def load_JHU_Data_global(st : str):
+def country_data(country : str, global_data = None):
+    if global_data is None:
+        global_data = Global_Data()
+
+    confirmed = global_data.confirmed_.copy()
+    deaths = global_data.deaths_.copy()
+    recs = global_data.recovered_.copy()
+    
     for df in [deaths, confirmed, recs]:
         # Filter Country
-        df.drop(df[ df["Country/Region"] != Settings.country].index, inplace=True)
+        df.drop(df[ df["Country/Region"] != country].index, inplace=True)
         # Drop Unused Columns:
         columns_to_delete = range(0, 5)
         df.drop(df.columns[columns_to_delete], axis=1, inplace=True)
@@ -122,19 +124,44 @@ def USA_population():
 
 #---------------------------------------------------------------------
 
-def growth_pcnt(ser): 
-    return (ser - ser.shift(1)) / ser * 100
 
 class COVID_19_Stats:
-    def __init__(self, region: str):
+    def __init__(self, region: str, region_data : pd.DataFrame):
         self.region_ = region
-        self.load()
+        self.cv_data_ = region_data
+        
         self.confirmed_new_ = (self.cv_data_.Confirmed - self.cv_data_.Confirmed.shift(1)).dropna()
         self.deaths_new_ = (self.cv_data_.Dead - self.cv_data_.Dead.shift(1)).dropna()
+        def growth_pcnt(ser): 
+            return (ser - ser.shift(1)) / ser * 100
         self.deaths_new_pcnt_ = growth_pcnt(self.cv_data_.Dead)
         self.confirmed_new_pcnt_ = growth_pcnt(self.cv_data_.Confirmed)
 
        
+    def added_new_cases(self):
+        if "Recovered" not in self.cv_data_:
+            return
+        self.recovered_new_ = (self.cv_data_.Recovered - self.cv_data_.Recovered.shift(1)).dropna()
+
+        growth = pd.Series((self.confirmed_new_ - (self.deaths_new_ + self.recovered_new_)).array
+                           , index=self.cv_data_.index[1:]).dropna()
+
+        print(growth.tail(5))
+
+        growth.plot(figsize=(14, 6)
+                    , title=f'{self.region_} Added New Cases - full range'
+                    , legend=True
+                    , label="New Cases"
+                    , style='.-')
+        growth.rolling(30).mean().plot(grid=True, legend=True, label="30 days MA")
+        plt.show()
+
+
+        df_data = self.deaths_new_[60:].dropna()
+        df_ma = self.deaths_new_[30:].rolling(30).mean().dropna()
+
+        print("Deaths Per Day\n" + str(self.deaths_new_.tail()))
+
     def deaths_additions(self):
         plt.figure(figsize = (15,10))
 
@@ -185,18 +212,8 @@ class COVID_19_Stats:
                               , "Daily Cases Addition (%)" : self.confirmed_new_pcnt_}
                           , index=self.deaths_new_pcnt_.index)['2020-03-01':]
 
-#         df.plot(title = "COVID-19 Dynamics (%)"
-#                 , grid = True
-#                 , legend = True
-#                 , style = '-o'
-#                 , figsize=(14, 6)
-#                 , color = ["darkred", "darkblue"]
-#                )
-
         # Last N days
-
         days = 90
-#         df = df[-days:]
         df[-days:].plot(title = f'{self.region_} COVID-19 Dynamics - last {days} days (%)'
                 , grid = True
                 , legend = True
@@ -213,44 +230,9 @@ class COVID_19_Stats:
         self.cases_additions()
         self.dynamics()
 
-class Country_COVID19_Stats(COVID_19_Stats):
-    def __init__(self, country: str):
-        super(Country_COVID19_Stats, self).__init__(country)
-    
-    def load(self):
-        self.cv_data_ = load_JHU_Data_global(self.region_)
+def Country_COVID19_Stats(country: str):
+    return COVID_19_Stats(country, country_data(country))
 
-    def added_new_cases(self):
-        self.recovered_new_ = (self.cv_data_.Recovered - self.cv_data_.Recovered.shift(1)).dropna()
-
-        growth = pd.Series((self.confirmed_new_ - (self.deaths_new_ + self.recovered_new_)).array
-                           , index=self.cv_data_.index[1:]).dropna()
-
-        print(growth.tail(5))
-
-        growth.plot(figsize=(14, 6)
-                    , title=f'{self.region_} Added New Cases - full range'
-                    , legend=True
-                    , label="New Cases"
-                    , style='.-')
-        growth.rolling(30).mean().plot(grid=True, legend=True, label="30 days MA")
-        plt.show()
-
-
-
-        df_data = self.deaths_new_[60:].dropna()
-        df_ma = self.deaths_new_[30:].rolling(30).mean().dropna()
-
-        print("Deaths Per Day\n" + str(self.deaths_new_.tail()))
-
-class USA_State_COVID19_Stats(COVID_19_Stats):
-    def __init__(self, state: str):
-        super(USA_State_COVID19_Stats, self).__init__(state)
-
-    def load(self):
-        self.cv_data_ = load_JHU_Data_USA(self.region_)
-
-    def added_new_cases(self):
-        # no data for numbers of recovered cases (Yet?) TODO
-        pass
+def USA_State_COVID19_Stats(state : str):
+    return COVID_19_Stats(state, usa_state_data(state))
     
